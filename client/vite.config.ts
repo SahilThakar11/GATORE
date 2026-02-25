1d00aacb6315791ea86d883321626f205762adc5
Justin <blizzorbsorc@gmail.com>
2026-02-25 10:49:14 -0500

    Add server proxy configuration for API integration for frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,21 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000", // Proxy to the backend server
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy) => {
+        proxy.on("error", (err, _req, _res) => {
+          console.log("proxy error", err);
+        });
+        proxy.on("proxyReq", (proxyReq, req, _res) => {
+          console.log("proxying", req.method, req.url, "->", proxyReq.getHeader("host"));
+        });
+      },
+      },
+    },
+  },
 });
