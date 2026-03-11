4dded24d0ad8d82ea8533d947b6ddea10d0a8165
SahilThakar11 <thakars311@gmail.com>
2026-03-10 23:47:23 -0400

    Created Preferences for the users to recommend games

--- server/src/middleware/validation.ts
+++ server/src/middleware/validation.ts
@@ -15,7 +15,15 @@ export const validateSignupComplete = [
     .withMessage("Valid email is required"),
   body("password")
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters"),
+    .withMessage("Password must be at least 8 characters")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage("Password must contain at least one special character"),
 ];
 
 export const validateGuestSignup = [
